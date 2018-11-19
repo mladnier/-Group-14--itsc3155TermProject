105ab69b3fa5335b466df493d6ead0bbcc33c551
jkeisler <jkeisler@uncc.edu>
2018-11-19 00:53:22 +0000

    Testing with setting dog id

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,11 @@ module SessionsHelper
     def log_in(dog)
     session[:dog_id] = dog.id
     end
+    
+    def current_user
+    if session[:dog_id]
+      @current_dog ||= Dog.find_by(id: session[:dog_id])
+    end
+  end
+    
 end
