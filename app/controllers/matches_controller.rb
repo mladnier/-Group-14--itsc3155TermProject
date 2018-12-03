99c41bcaebc9543c77e4f0b1bda4da52ae77b8ee
jkeisler <jkeisler@uncc.edu>
2018-12-03 05:05:58 +0000

    Added a function to not show yourown dog and dogs youve patched with

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,20 +1,28 @@
 class MatchesController < ApplicationController
     
     def index
-        
-       @mycurrentmatch = Dog.find_by_id(1+rand(Dog.count))
-       
-       
-       
-       @mymatchid = @mycurrentmatch.id
+      
        @currentdog = Dog.find_by(@current_user_id)
        @currentdogid =  @currentdog.id
        
+       loop do 
+             @mycurrentmatch = Dog.find_by_id(1+rand(Dog.count))
+             @mymatchid = @mycurrentmatch.id
+            
+             break if @mymatchid != @currentdogid && !(Match.exists?(:dogtwo => @mymatchid ))
+            
+        end
+    
     end
+    
+    
     
     def create 
         @match = Match.create(match_params)
         @match.save
+        redirect_to dog_matches_path
+        
+       
     end
    
     
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,4 @@
 class Match < ApplicationRecord
   
+  
 end
