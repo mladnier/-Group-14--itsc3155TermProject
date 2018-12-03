2e4268d604eab7a4a5cd6dd6cc3495110d9f838f
Jonathan Chickie <jchickie@uncc.edu>
2018-12-03 02:12:51 +0000

    Add styling for a lot of stuff and add carousel

---
+++ app/helpers/carousel_helper.rb
@@ -0,0 +1,34 @@
+module CarouselHelper
+  def carousel_for(images)
+    Carousel.new(self, images).html
+  end
+
+  class Carousel
+    def initialize(view, images)
+      @view, @images = view, images
+    end
+
+   def html
+      content = view.safe_join([slides])
+      view.content_tag(:div, content, class: 'carousel slide', :data => {:ride => 'carousel'})
+   end
+
+    private
+
+    attr_accessor :view, :images
+    delegate :link_to, :content_tag, :image_tag, :safe_join, to: :view
+
+    def slides
+      items = images.map.with_index { |image, index| slide_tag(image, index.zero?) }
+      content_tag(:div, safe_join(items), class: 'carousel-inner')
+    end
+
+    def slide_tag(image, is_active)
+      options = {
+        class: (is_active ? 'carousel-item active' : 'carousel-item'),
+      }
+
+      content_tag(:div, image_tag(image), options)
+    end
+  end
+end
